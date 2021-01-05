1a06b8f29acacff98f03e2620ccc9e95ecde602e
BourdonAlexis <alexisbourdon@hotmail.fr>
2021-01-05 08:47:16 +0100

    Ajout exo 2 - 6

---
+++ Eval sql/Exercice 2 - 6 - Pays dont les clients ont passe commande de produits fournis par  Exotic Liquids.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT customers.Country AS 'Pays'
+FROM customers 
+JOIN orders
+ON orders.CustomerID = customers.CustomerID 
+JOIN order_details
+ON orders.OrderID = order_details.OrderID
+JOIN products
+ON order_details.ProductID = products.ProductID
+JOIN suppliers
+ON products.SupplierID = suppliers.SupplierID
+WHERE suppliers.CompanyName = 'Exotic Liquids' ;
