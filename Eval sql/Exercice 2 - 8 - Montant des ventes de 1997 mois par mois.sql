31c2e063ec9cb8c241b3637151d35e7d5e561cbd
BourdonAlexis <alexisbourdon@hotmail.fr>
2021-01-06 13:11:35 +0100

    Affichage par mois + bon compte

--- Eval sql/Exercice 2 - 8 - Montant des ventes de 1997 mois par mois.sql
+++ Eval sql/Exercice 2 - 8 - Montant des ventes de 1997 mois par mois.sql
@@ -1,4 +1,6 @@
-SELECT MONTH(OrderDate) AS 'Mois 97' ,SUM(Freight) AS 'Montant de vente'
+SELECT MONTH(OrderDate) AS 'Mois 97' ,SUM(order_details.UnitPrice*order_details.Quantity) AS 'Montant de vente'
 FROM orders 
+JOIN order_details
+ON orders.OrderID = order_details.OrderID
 WHERE OrderDate LIKE '1997%'
-GROUP BY OrderID;
+GROUP BY MONTH(OrderDate);
